e24656d18444c1a1899048b1089f05c900746d93
Ritik Jain <300073191@BLRM-300073191.local>
2023-05-31 18:04:05 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles.css'
+import React from 'react'
+export const App = () => {
+  return (
+    <React.Fragment>
+      <h1>
+        React typescript webpack starter - {process.env.NODE_ENV}{' '}
+        {process.env.name}
+      </h1>
+    </React.Fragment>
+  )
+}
